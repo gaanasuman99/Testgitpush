e525e81f3ce7045ac82ec8cd6df3060a64e06bdd
gaanasuman99 <gaanasuman22@gmail.com>
2023-01-14 16:57:34 +0530

    This is my first code push to git.

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://gaanasuman:gaana123@cluster0.zjkpg6v.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"gaana",
+    "mail":"gaanasuman22gmail.com",
+     "surname":"suman"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
